dbf056f8b5b8724ab50bf0c6bc9fa20cb26c8595
KaiYit <kaiyit812@gmail.com>
2021-08-22 16:26:31 +0800

    Update

---
+++ app/src/main/java/my/edu/tarc/ok4umobile/AdminEventDetails.kt
@@ -0,0 +1,25 @@
+package my.edu.tarc.ok4umobile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AdminEventDetails : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_event_details, container, false)
+    }
+
+
+}
