76c96d4feeb8a04a640c23f40a341afdd16607a1
handsomekent <59521336+handsomekent@users.noreply.github.com>
2021-08-28 19:09:42 +0800

    backp

--- app/src/main/java/my/edu/tarc/ok4umobile/MainActivity.kt
+++ app/src/main/java/my/edu/tarc/ok4umobile/MainActivity.kt
@@ -1,11 +1,27 @@
 package my.edu.tarc.ok4umobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1=findViewById<Button>(R.id.button2)
+        val btn2=findViewById<Button>(R.id.button3)
+
+        btn1.setOnClickListener{
+            val i= Intent(this,Drawer::class.java)
+            startActivity(i)
+        }
+        btn1.setOnClickListener{
+            val i= Intent(this,Drawer::class.java)
+            startActivity(i)
+        }
+
+
     }
 }
