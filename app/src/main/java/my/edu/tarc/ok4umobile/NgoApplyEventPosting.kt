1d7bbb42301ceb7d4579cda6c36c240e07cbf32b
robin000621 <tankyr-cm18@student.tarc.edu.my>
2021-09-18 23:10:44 +0800

    newest

--- app/src/main/java/my/edu/tarc/ok4umobile/NgoApplyEventPosting.kt
+++ app/src/main/java/my/edu/tarc/ok4umobile/NgoApplyEventPosting.kt
@@ -1,59 +1,104 @@
 package my.edu.tarc.ok4umobile
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import my.edu.tarc.ok4umobile.databinding.FragmentNgoApplyEventPostingFragmenttBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ApplyEventPostingFragmentt.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ApplyEventPostingFragmentt : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var binding: FragmentNgoApplyEventPostingFragmenttBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ngo_apply_event_posting_fragmentt, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ApplyEventPostingFragmentt.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ApplyEventPostingFragmentt().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_ngo_apply_event_posting_fragmentt,
+            container,
+            false
+        )
+
+        val sharedPref = this.activity?.getSharedPreferences(
+            "pref", Context.MODE_PRIVATE
+        )
+        var name: String = sharedPref?.getString("name", "No Data").toString()
+        var email: String = sharedPref?.getString("email", "No Data").toString()
+
+        val databaseuser =
+            FirebaseDatabase.getInstance("https://ok4u-a1047-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                .getReference("Users")
+
+        binding.btnApply.setOnClickListener(){
+            val pass : String = binding.txtPassword.text.toString()
+            val conf : String = binding.txtConfirm.text.toString()
+
+            if(pass.compareTo(conf) == 0){
+                val name : String = binding.txtName.text.toString()
+                val email : String = binding.txtEmail.text.toString()
+                val temp : Int  = binding.radGender.checkedRadioButtonId
+                val rad = view?.findViewById<RadioButton>(temp)
+                val gender = rad?.text.toString()
+                val address=binding.txtAddress.text.toString()
+                val phoneNum=binding.txtPhoneNum.text.toString()
+                val radio_user_type:Int =binding.radUserType.checkedRadioButtonId
+                val radio_usertype= view?.findViewById<RadioButton>(radio_user_type)
+                val user_type= radio_usertype?.text.toString()
+
+
+                // val database = Firebase.database("https://ok4u-bc86a-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                val database = Firebase.database("https://ok4u-a1047-default-rtdb.asia-southeast1.firebasedatabase.app/")
+                val ref = database.getReference("Users")
+
+
+                // val newuser = User("003", name, gender, pass, email,user_type)
+                // ref.child("003").setValue(newuser)
+                val newuser = User(name, gender, pass, email,address,phoneNum,user_type)
+
+
+
+                ref.child(email).setValue(newuser)
+                Toast.makeText(this.context, "Register Success", Toast.LENGTH_LONG).show()
+
+                Navigation.findNavController(it).navigate(R.id.action_registerFragment_to_loginFragment)
+            }else{
+                Toast.makeText(this.context, "Incorrect Password", Toast.LENGTH_SHORT).show()
             }
+
+
+            //database.child("users").child("001").setValue(newuser)
+        }
+
+        )
+
+
+        return binding.root
+
+
     }
 }
