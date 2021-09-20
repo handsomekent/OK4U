b8e7e33679ae1ec4d296d68f623b71bd0daf4f8a
wang3081 <77950891+wang3081@users.noreply.github.com>
2021-09-21 01:33:38 +0800

    participatefragment
    participatefragment

--- app/src/main/java/my/edu/tarc/ok4umobile/NgoEventFragment.kt
+++ app/src/main/java/my/edu/tarc/ok4umobile/NgoEventFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.auth.User
+import my.edu.tarc.ok4umobile.databinding.FragmentLoginBinding
 
 
 class NgoEventFragment : Fragment() {
+
+
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
 
         return inflater.inflate(R.layout.fragment_ngo_event, container, false)
     }
--- app/src/main/java/my/edu/tarc/ok4umobile/ParticipateFragment.kt
+++ app/src/main/java/my/edu/tarc/ok4umobile/ParticipateFragment.kt
@@ -5,17 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.auth.User
 import my.edu.tarc.ok4umobile.R
+import my.edu.tarc.ok4umobile.databinding.FragmentLoginBinding
+import my.edu.tarc.ok4umobile.databinding.FragmentParticipateBinding
 
 class ParticipateFragment : Fragment() {
-
+    private lateinit var binding: FragmentParticipateBinding
+    private lateinit var userList: ArrayList<User>
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_participate, container, false)
+        binding = inflate(inflater, R.layout.fragment_participate, container, false)
+        val database = FirebaseDatabase.getInstance("https://ok4u-a1047-default-rtdb.asia-southeast1.firebasedatabase.app/").getReference("Users")
+
+
+
+        return binding.root
     }
 
 }
